79153e1d6028c2039253e034c71844df94642428
Xu Dapeng <xustev75@gmail.com>
2021-01-24 15:20:08 -0800

    docker change add google cloud translate

--- test/translator.py
+++ test/translator.py
@@ -7,5 +7,18 @@ def test_translator():
     result = translator.translate(text)
     assert (
         result
-        == "The collapse of Lehman Brothers had nothing to do with the fall of the Berlin Wall"
+        == "The collapse of Lehman Brothers has nothing to do with the fall of the Berlin Wall"
     )
+
+# google cloud test
+def test_translator_GC():
+    text = "雷曼兄弟公司的倒闭和柏林墙的倒塌没有任何关系"
+    translator = Translator(engine="gc")
+    result = translator.translate(text)
+    
+    assert (
+        result
+        == "The collapse of Lehman Brothers has nothing to do with the fall of the Berlin Wall"
+    )
+
+
