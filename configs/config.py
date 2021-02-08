65ad63ec0b0d60461ca4ca18acb3edd54e9f74c2
Steven <xustev75@gmail.com>
2021-02-08 14:44:52 -0500

    add def for generating project config

--- configs/config.py
+++ configs/config.py
@@ -31,3 +31,21 @@ def get_report_path(filename):
     reportfile_path = "{dir}/report.{filename}.csv".format(**locals())
     return reportfile_path
 
+def generate_docs_config(docs_array, source_lang_code, target_lang_code):
+    return [
+            {
+                "sourceLanguageCode": source_lang_code,
+                "targetLanguageCode": target_lang_code,
+                "file": doc
+            }
+            for doc in docs_array
+            ]
+
+def generate_project_config(name, engine, docs_array, source_lang_code, target_lang_code):
+    return {
+            "name": name,
+            "engine": engine,
+            "docs": generate_docs_config(docs_array, source_lang_code, target_lang_code),
+            }
+
+
--- test/docx_processor.py
+++ test/docx_processor.py
@@ -1,8 +1,69 @@
 from ..src.docx_processor import DocxProcessor
+from ..configs.config import generate_project_config
 import pytest
 
-@pytest.mark.skip(reason="Demo 01")
-def test_docx_processor():
+def test_docx_processor(project_config_pro3):
+
+    project = project_config_pro3
+    for doc in project["docs"]:
+        processor = DocxProcessor(
+            doc["file"],
+            engine=project["engine"],
+            sourceLanguageCode=doc["sourceLanguageCode"],
+            targetLanguageCode=doc["targetLanguageCode"],
+        )
+        result = processor.translate_doc()
+        assert result == True
+
+
+def test_fixture():
+    docs=["fil1.docx", "file2.docx"]
+    project = generate_project_config("test_fixture", "gc", docs, "zh-cn", "en")
+    assert project["name"] == "test_fixture"
+    assert project["engine"] == 'aws'
+    assert project["docs"][0]["sourceLanguageCode"] == 'zh-cn'
+    assert project["docs"][1]["sourceLanguageCode"] == 'zh-cn'
+
+
+@pytest.fixture
+def project_config_pro3():
+    docs = [
+            '2.4 非临床研究概述-LY03009-20210204.docx',
+            '2.6 非临床试验文字和列表总结-LY03009-20210122.docx',
+            '2.6.1 前言-LY03009-20210122.docx',
+            '2.6.2 药理学文字总结-汇总-LY03009-20210122.docx',
+            '2.6.3 药理学列表总结-LY03009-20210122.docx',
+            '2.6.6 毒理学文字总结 -LY03009-20210122.docx',
+            '2.6.7 毒理学试验列表-LY03009-20210122.doc'
+    ]
+
+    return generate_project_config("project 3", "gc", docs, 'zh-cn', 'en')
+
+
+
+@pytest.fixture
+def project_config_gc():
+    
+    project = {
+        "name": "demo 01",
+        "engine": "gc",
+        "docs": [
+            {
+                "sourceLanguageCode": "zh-cn",
+                "targetLanguageCode": "en",
+                "file": "NC PD Report Sample.docx",
+            },
+            {
+                "sourceLanguageCode": "en",
+                "targetLanguageCode": "zh-cn",
+                "file": "NC Tox Report Sample.docx",
+            },
+        ],
+    }
+    return project
+
+@pytest.fixture
+def project_config_pro1():
     project = {
         "name": "demo 01",
         "engine": "aws",
@@ -29,19 +90,10 @@ def test_docx_processor():
             },
         ],
     }
+    return project
 
-    for doc in project["docs"]:
-        processor = DocxProcessor(
-            doc["file"],
-            engine=project["engine"],
-            sourceLanguageCode=doc["sourceLanguageCode"],
-            targetLanguageCode=doc["targetLanguageCode"],
-        )
-        result = processor.translate_doc()
-        assert result == True
-
-
-def test_docx_processor_demo2():
+@pytest.fixture
+def project_config_pro2():
     project = {
         "name": "demo 02",
         "engine": "gc",
@@ -79,44 +131,5 @@ def test_docx_processor_demo2():
 
         ],
     }
-
-    for doc in project["docs"]:
-        processor = DocxProcessor(
-            doc["file"],
-            engine=project["engine"],
-            sourceLanguageCode=doc["sourceLanguageCode"],
-            targetLanguageCode=doc["targetLanguageCode"],
-        )
-        result = processor.translate_doc()
-        assert result == True
-
-
-@pytest.mark.skip(reason="test GC")
-def test_docx_processor_gc():
-    project = {
-        "name": "demo 01",
-        "engine": "gc",
-        "docs": [
-            {
-                "sourceLanguageCode": "zh-cn",
-                "targetLanguageCode": "en",
-                "file": "NC PD Report Sample.docx",
-            },
-            {
-                "sourceLanguageCode": "en",
-                "targetLanguageCode": "zh-cn",
-                "file": "NC Tox Report Sample.docx",
-            },
-        ],
-    }
-
-    for doc in project["docs"]:
-        processor = DocxProcessor(
-            doc["file"],
-            engine=project["engine"],
-            sourceLanguageCode=doc["sourceLanguageCode"],
-            targetLanguageCode=doc["targetLanguageCode"],
-        )
-        result = processor.translate_doc()
-        assert result == True
+    return project
 
